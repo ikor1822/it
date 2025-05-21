e70756351063f824e2e75b3719b4d71e362600f1
Matvey <98540142+smileMatvey27@users.noreply.github.com>
2025-05-21 21:16:51 +0300

    Create BD.py

---
+++ BD.py
@@ -0,0 +1,28 @@
+import sqlite3
+import csv
+
+conn = sqlite3.connect('shedule.db')
+cursor = conn.cursor()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS schedule (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        subject TEXT,
+        date TEXT,
+        time TEXT,
+        lesson_type TEXT,
+        teacher TEXT,
+        classroom TEXT,
+        group_name TEXT
+    )
+''')
+with open('schedule.csv', 'r', encoding='utf-8') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=';')
+    next(csv_reader)
+    insert_query = '''
+        INSERT INTO schedule (subject, date, time, lesson_type, teacher, classroom, group_name)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+    '''
+    for row in csv_reader:
+        cursor.execute(insert_query, row)
+conn.commit()
+conn.close()

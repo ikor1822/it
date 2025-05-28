7f4e63ac33961a993104defb6a160d5e91d108bd
Matvey <98540142+smileMatvey27@users.noreply.github.com>
2025-05-28 10:28:03 +0300

    Update BD.py

--- BD.py
+++ BD.py
@@ -1,7 +1,7 @@
 import sqlite3
 import csv
 
-conn = sqlite3.connect('sсhedule.db')
+conn = sqlite3.connect('schedule.db')
 cursor = conn.cursor()
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS schedule (
